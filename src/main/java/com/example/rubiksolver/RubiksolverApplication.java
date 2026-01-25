d3e14e509a061006c58a26b7913a1e013cc54f80
RajS45 <raj23160484@gmail.com>
2026-01-25 11:59:13 +0530

    Initial commit

---
+++ src/main/java/com/example/rubiksolver/RubiksolverApplication.java
@@ -0,0 +1,13 @@
+package com.example.rubiksolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RubiksolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RubiksolverApplication.class, args);
+	}
+
+}
